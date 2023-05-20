a4542e22ba79ff931fb99af498752edc28ebb8c6
Danila Emelyanov <62074408+jezmunh@users.noreply.github.com>
2023-05-20 14:53:25 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 require "lib/rb.php";
 R::setup( 'mysql:host=localhost;dbname=shortlink',
-'root', 'root' ); //for both mysql or mariaDB
+'user', 'password' ); //for both mysql or mariaDB
 session_start();
 
 
